e34548528df6646de4332685bf5b9a8c692d4d4c
Alexandra Aversani <alexandraaversani.tds2.senai@gmail.com>
2025-04-03 14:21:57 -0300

    feat: componentização da footer

--- meme-verse/src/app/page.jsx
+++ meme-verse/src/app/page.jsx
@@ -1,15 +1,15 @@
 // import styles from './page.module.css'
 import Header from './components/header';
+import Footer from './components/footer';
 
 
-// import SecaoCategorias from './components/secaoCategorias';
-// import SecaoLancamentos from './components/secaoLancamentos';
-// import SecaoLivros from './components/secaoLivros';
 
 export default function Home() {
   return (
     <div >
       <Header />
+      <Footer />
+
    
       
     </div>
