338933ec850bb27060e15f21847cd174ca1247f0
Alexandra Aversani <alexandraaversani.tds2.senai@gmail.com>
2025-04-03 14:30:43 -0300

    feat: componentização do herosection

--- meme-verse/src/app/page.jsx
+++ meme-verse/src/app/page.jsx
@@ -1,5 +1,6 @@
 // import styles from './page.module.css'
 import Header from './components/header';
+import HeroSection from './components/heroSection';
 import Footer from './components/footer';
 
 
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <div >
       <Header />
+      <HeroSection />
       <Footer />
 
    
