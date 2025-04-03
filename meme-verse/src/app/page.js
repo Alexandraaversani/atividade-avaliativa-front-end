791c0cf86c35ce2c1ae7e267643013e8fd179bcb
Alexandra Aversani <alexandraaversani.tds2.senai@gmail.com>
2025-04-03 14:07:13 -0300

    feat: criação das pastas e da header

---
+++ meme-verse/src/app/page.js
@@ -0,0 +1,17 @@
+// import styles from './page.module.css'
+import Header from './components/header';
+
+
+// import SecaoCategorias from './components/secaoCategorias';
+// import SecaoLancamentos from './components/secaoLancamentos';
+// import SecaoLivros from './components/secaoLivros';
+
+export default function Home() {
+  return (
+    <div >
+      <Header />
+   
+      
+    </div>
+  );
+}
