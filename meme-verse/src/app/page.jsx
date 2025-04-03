7c3482245dfe92a3b100e136a5c44c8ae557fec2
Alexandra Aversani <alexandraaversani.tds2.senai@gmail.com>
2025-04-03 16:23:23 -0300

    feat: ajustes

--- meme-verse/src/app/page.jsx
+++ meme-verse/src/app/page.jsx
@@ -1,6 +1,10 @@
 // import styles from './page.module.css'
 import Header from './components/header';
 import HeroSection from './components/heroSection';
+import CategorieSection from './components/categorieSection';
+import MemeCard from './components/memeCard';
+import Sidebar from './components/sidebar';
+import InteractionBar from './components/interactionBar';
 import Footer from './components/footer';
 
 
@@ -10,6 +14,10 @@ export default function Home() {
     <div >
       <Header />
       <HeroSection />
+      <CategorieSection />
+      <MemeCard />
+      <Sidebar />
+      <InteractionBar />
       <Footer />
 
    
